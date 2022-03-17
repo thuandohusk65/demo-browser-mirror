97f047d0b6b3c899146df44259794e0a8b47918b
QuocBao <quocbaovu15101996@gmail.com>
2022-03-17 18:11:35 +0700

    fix: lỗi showrate của ads

--- app/src/main/java/com/nhnextsoft/screenmirroring/view/activity/SettingActivity.kt
+++ app/src/main/java/com/nhnextsoft/screenmirroring/view/activity/SettingActivity.kt
@@ -14,8 +14,10 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.nhnextsoft.control.Admod
+import com.nhnextsoft.control.dialog.PrepareLoadingAdsDialog
 import com.nhnextsoft.control.funtion.AdCallback
 import com.nhnextsoft.screenmirroring.BuildConfig
 import com.nhnextsoft.screenmirroring.R
@@ -24,15 +26,19 @@ import com.nhnextsoft.screenmirroring.config.AppConfigRemote
 import com.nhnextsoft.screenmirroring.config.AppPreferences
 import com.nhnextsoft.screenmirroring.databinding.ActivitySettingBinding
 import com.nhnextsoft.screenmirroring.utility.extensions.isNetworkAvailable
+import timber.log.Timber
 import java.util.*
 
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
+    private lateinit var modalLoadingAd: PrepareLoadingAdsDialog
+    private var isLoadedAdInterstitial: Boolean = false;
     private var mInterstitialAd: InterstitialAd? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingBinding.inflate(layoutInflater)
+        modalLoadingAd = PrepareLoadingAdsDialog(this)
         initView()
         setContentView(binding.root)
         loadAdInterstitial()
@@ -45,6 +51,11 @@ class SettingActivity : AppCompatActivity() {
                 override fun onInterstitialLoad(interstitialAd: InterstitialAd?) {
                     super.onInterstitialLoad(interstitialAd)
                     mInterstitialAd = interstitialAd
+                    isLoadedAdInterstitial = true
+                }
+                override fun onAdFailedToLoad(i: LoadAdError?) {
+                    super.onAdFailedToLoad(i)
+                    isLoadedAdInterstitial = true
                 }
             })
     }
@@ -208,20 +219,45 @@ class SettingActivity : AppCompatActivity() {
         }
     }
 
+    private fun showAd(): Unit {
+        Admod.instance?.forceShowInterstitial(
+            this,
+            mInterstitialAd,
+            object : AdCallback() {
+                override fun onAdClosed() {
+                    super.onAdClosed()
+                    finish()
+                }
+            }
+        )
+    }
     override fun onBackPressed() {
+        Timber.d("onBackPressed"+ isLoadedAdInterstitial)
         if (!isNetworkAvailable()) {
             finish()
         } else {
-            Admod.instance?.showInterstitialAdByTimes(
-                this,
-                mInterstitialAd,
-                object : AdCallback() {
-                    override fun onAdClosed() {
-                        super.onAdClosed()
-                        finish()
-                    }
-                }
-            )
+            if (isLoadedAdInterstitial) {
+                showAd()
+            } else {
+                modalLoadingAd.show()
+                Admod.instance?.getInterstitalAds(this,
+                    AdConfig.AD_ADMOB_CLOSE_BACK_HOME_INTERSTITIAL,
+                    object : AdCallback() {
+                        override fun onInterstitialLoad(interstitialAd: InterstitialAd?) {
+                            super.onInterstitialLoad(interstitialAd)
+                            mInterstitialAd = interstitialAd
+                            isLoadedAdInterstitial = true
+                            modalLoadingAd.dismiss()
+                            showAd()
+                        }
+                        override fun onAdFailedToLoad(i: LoadAdError?) {
+                            super.onAdFailedToLoad(i)
+                            isLoadedAdInterstitial = true
+                            modalLoadingAd.dismiss()
+                            finish()
+                        }
+                    })
+            }
         }
     }
 
